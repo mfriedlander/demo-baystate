23b45c470030cd938e9f8f0f50a5f05d765e1fec
Matt Friedlander <mfriedlander@hhogdev.com>
2016-05-19 13:32:11 -0400

    Added test project and mock implementation

---
+++ BaystateHealth.Business.Services.WaitTimes.Test/WaitTimesProviderTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaystateHealth.Business.Services.WaitTimes.Test
+{
+    [TestClass]
+    public class WaitTimesProviderTest
+    {
+        private IWaitTimeProvider _provider;
+        private readonly string VALIDKEY;
+        private readonly string INVALIDKEY;
+
+        public WaitTimesProviderTest()
+        {
+            // Normally handled via dependecy injection
+            _provider = new Mock.MockWaitTimeProvider();
+            VALIDKEY = "1";
+            INVALIDKEY = "999";
+        }
+
+        [TestMethod]
+        public void Get_Single_Valid_Key()
+        {
+            WaitTime waitTime = _provider.GetWaitTime(VALIDKEY);
+
+            Assert.IsNotNull(waitTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Get_Single_Invalid_Key()
+        {
+            WaitTime waitTime = _provider.GetWaitTime(INVALIDKEY);
+        }
+
+        [TestMethod]
+        public void Get_All()
+        {
+            IEnumerable<WaitTime> waitTimes = _provider.GetWaitTimes();
+
+            Assert.IsTrue(waitTimes.Any());
+        }
+    }
+}
